e916b0eb0226d8420f64e992ad09e4e8953b4598
Pierre <pierre@a-ll.tech>
2025-09-02 20:21:09 +0200

    add token

--- app/lib/strapi.js
+++ app/lib/strapi.js
@@ -1,5 +1,5 @@
 const STRAPI_URL = process.env.STRAPI_URL || process.env.NEXT_PUBLIC_STRAPI_URL;
-const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
+const STRAPI_TOKEN = process.env.STRAPI_API_TOKEN;
 
 function buildUrl(path, params = {}) {
   const usp = new URLSearchParams();
