63ace826434ce544775b9b2847ea7bb4062efc75
Pierre <pierre@a-ll.tech>
2025-09-02 20:38:05 +0200

    remove settings

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -2,17 +2,13 @@ import { notFound } from "next/navigation";
 import { locales } from "../i18n";
 import Navigation from "../components/navigation/Navigation";
 import { getNavigation } from "../lib/strapi";
-import { getSiteSettings } from "../lib/strapi";
 import Footer from "../components/footer/Footer";
 import SmoothScrolling from "../components/scroll/SmoothScrolling";
 import ScrollToTopLenis from "../components/scroll/ScrollToTopLenis";
 import IllustrationList from "../components/Illustrations/IllustrationList";
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
-  const globals = await getSiteSettings({ locale });
   if (!locales.includes(locale)) notFound();
-
-  // "main" = slug/nom de ton container dans Strapi (adapte si nécessaire)
   const nav = await getNavigation({ tree: "navigation", locale, type: "TREE" });
 
   return (
