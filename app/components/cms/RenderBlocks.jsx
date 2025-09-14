ecb93788b59b006303a2a2eda2a2795289929394
Pierre <pierre.hayoz95@gmail.com>
2025-09-14 18:13:58 +0200

    translate breadcrumb

--- app/components/cms/RenderBlocks.jsx
+++ app/components/cms/RenderBlocks.jsx
@@ -164,8 +164,18 @@ export default async function RenderBlocks({ blocks = [], locale, searchParams }
           </div>);
         break;
       }
-      case 'blocks.map':
-      return <ContactMap lat={b.latitude} lng={b.longitude} label={b.label}/>;
+
+      case 'blocks.map': {
+  out.push(
+    <div key={`map-${b.id ?? `idx-${idx}`}`} className="grid grid-cols-4 px-4 py-8">
+      <div className="col-span-4">
+        <ContactMap lat={b.latitude} lng={b.longitude} label={b.label} />
+      </div>
+    </div>
+  );
+  break;
+}
+
       case "blocks.paragraphes": {
         out.push(
           <div key={`paragraphes-${b.id ?? `idx-${idx}`}`} className="grid grid-cols-4 pb-8">
