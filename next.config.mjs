085953578328361c15db05c1976321f6f459c2c2
Pierre <pierre@a-ll.tech>
2025-12-15 12:46:31 +0100

    delete some files

--- next.config.mjs
+++ next.config.mjs
@@ -10,24 +10,13 @@ const nextConfig = {
         port: '',
         pathname: '/uploads/**', // ou '/**' si besoin
       },
-      // (optionnel) si certaines images peuvent aussi venir du staging (uploads proxifiés)
-      {
-        protocol: 'https',
-        hostname: 'staging.miam.tipper.watch',
-        port: '',
-        pathname: '/uploads/**',
-      },
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1337',
         pathname: '/uploads/**',
       },
-      
-      // (optionnel) ajoute ici d’autres CDN (Cloudinary/S3...) si tu en utilises
-      // { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
     ],
-    // formats: ['image/avif','image/webp'], // optionnel
   },
 };
 
